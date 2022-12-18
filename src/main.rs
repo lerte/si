b32abbfc6b0c441b6358b1326ea147c5ae0c293a
Lerte Smith <lertesmith@gmail.com>
2022-12-18 14:23:01 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::time::{SystemTime};
+use std::env;
 use std::io::Write;
- 
+use std::time::{SystemTime};
+
+
 fn prompt(name:&str) -> String {
   let mut line = String::new();
   print!("{}", name);
@@ -9,16 +11,32 @@ fn prompt(name:&str) -> String {
 
   return line.trim().to_string()
 }
+
+fn repl() {
+  loop {
+    let input = prompt("-> ");
+    if input == "@now" {
+      let unixtime = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
+      print!("Current Unix time is {:?}\n", unixtime);
+    } 
+    else if input == "@exit" { 
+      std::process::exit(0);
+    };
+  }
+}
  
 fn main() {
-  loop {
-    let input=prompt("-> ");
-    if input=="now" {
-        let unixtime = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
-        print!("Current Unix time is {:?}\n", unixtime);
-    } 
-    else if input=="exit" { 
-        break; 
-    };
+  let args: Vec<String> = env::args().collect();
+  if args.len() == 1 {
+    repl()
+  }
+  match args[1].as_str() {
+    "version" => {
+      let version = env!("CARGO_PKG_VERSION");
+      println!("si version {}", &version);
+    }
+    _ => {
+      return;
+    }
   }
 }
