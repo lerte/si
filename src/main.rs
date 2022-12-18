09073c5c18b65c9810c07c3cda7e472f6240bde4
Lerte Smith <lertesmith@gmail.com>
2022-12-18 11:53:55 +0800

    init repo

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use std::time::{SystemTime};
+use std::io::Write;
+ 
+fn prompt(name:&str) -> String {
+  let mut line = String::new();
+  print!("{}", name);
+  std::io::stdout().flush().unwrap();
+  std::io::stdin().read_line(&mut line).expect("Error: Could not read a line");
+
+  return line.trim().to_string()
+}
+ 
+fn main() {
+  loop {
+    let input=prompt("-> ");
+    if input=="now" {
+        let unixtime = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
+        print!("Current Unix time is {:?}\n", unixtime);
+    } 
+    else if input=="exit" { 
+        break; 
+    };
+  }
+}
